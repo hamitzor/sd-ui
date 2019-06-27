6ab1b62ff110b174bebdcd869ffededd0d0bb8bd
Hamit Zor <thenrerise@gmail.com>
2019-06-27 11:46:12 +0300

    gitignore

--- app.config.default.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  domain: 'localhost',
-  port: 3000,
-  endpointUrl: {
-    session: 'localhost:8080/session'
-  }
-}
---
+++ load-api-config.js
@@ -0,0 +1,14 @@
+const fetch = require("cross-fetch")
+const fs = require("fs")
+
+const args = process.argv.slice(2)
+const loadApiConfig = async function () {
+  const apiConfig = (await (await fetch(args[0])).json()).payload
+  const content = `module.exports = ${JSON.stringify(apiConfig)}`
+  fs.writeFileSync("./default-api.config.js", content)
+  if (args[1] === "-c") {
+    fs.writeFileSync("./api.config.js", content)
+  }
+}
+
+loadApiConfig()
